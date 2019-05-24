b2b2ab3239aa4477e561400177d7b9d730437749
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2019-05-24 15:20:18 +0200

    Change routes for routing to react app

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
+
+  # This must be changed / something must be added for the API later
+  get '*path', to: 'main#index'
 end
