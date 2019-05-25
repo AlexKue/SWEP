1b708a8c2053af32c65956adea338c949ab58fc1
Alexander-Michael Kühnle <=>
2019-05-25 22:43:08 +0200

    Add controller methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,15 @@
 class Api::SessionsController < ApplicationController
+    
+    # POST /api/auth
+    def create
+        user = User.find_by(sessions[:user][:email].downcase)
+        if user && user.authenticate(sessions[:user][:password])
+            log_in user
+        end
+    end
+
+    # DELETE /api/logout
+    def destroy 
+        log_out if logged_in?
+    end
 end
