1883d7793aca86d0b0ee291138ccf1f8a87aa819
Alexander-Michael Kühnle <=>
2019-05-25 22:55:57 +0200

    Log in users after signup

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -18,6 +18,7 @@ class Api::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            log_in @user
             render json: @user, status: :created
         else
             render json: @user.errors, status: :unproccessable_entity
