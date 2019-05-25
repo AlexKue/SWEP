861d7369b32da673ed13d88aee6ced37634ea7df
Alexander-Michael Kühnle <=>
2019-05-25 16:19:09 +0200

    Add json response defaults

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
   
-  namespace :api do
-    resources :users
+  namespace :api, defaults: { format: 'json'} do
+    resources :users, except: [:new, :edit]
   end
 end
