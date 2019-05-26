8ad11c633e51b03c8fd46a5d85fc499b90d2ea4a
Alexander-Michael Kühnle <=>
2019-05-26 13:33:20 +0200

    Forward only non-Ajax to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
   
@@ -7,5 +6,9 @@ Rails.application.routes.draw do
     resources :users, except: [:new, :edit]
     post       "/auth",    to: "sessions#create"
     delete     "/logout",  to: "sessions#destroy"
+  end
+
+  get '*path', to: 'main#index', contraints: ->(req) do
+    !req.xhr? && req.format.html?
   end
 end
