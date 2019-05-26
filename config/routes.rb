b64698ec74b86e2b2b8c6ff575021bba5dd4ddda
Alexander-Michael Kühnle <=>
2019-05-26 12:59:01 +0200

    Changed auth past from get to post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: { format: 'json'} do
     resources :users, except: [:new, :edit]
-    get       "/auth",    to: "sessions#create"
-    delete    "/logout",  to: "sessions#destroy"
+    post       "/auth",    to: "sessions#create"
+    delete     "/logout",  to: "sessions#destroy"
   end
 end
