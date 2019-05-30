552b23571ae6d2db6baf3609a4431e8f16a94544
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2019-05-30 12:32:23 +0200

    Add register component

---
+++ app/javascript/components/Register/Register.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import API from '../API/API.jsx';
+
+export default class Register extends React.Component {
+
+  render() {
+    return (
+      <h1>Hello, Register!</h1>
+    );
+  }
+}
