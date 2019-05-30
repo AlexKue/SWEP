87a0eefa358080d964e6beb738f5b172f3a6e557
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-05-30 13:49:07 +0200

    Allow only users to show index

--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -11,4 +11,20 @@ class Api::UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_response :no_content
   end
+
+  test "should return unauthorized if not logged in" do
+    get api_users_path
+    assert_response :unauthorized
+  end
+
+  test "should show index after logged in" do
+    post api_auth_path params: {
+      session: {
+        email: @user.email,
+        password: "password"
+      }
+    }
+    get api_users_path
+    assert_response :ok
+  end
 end
