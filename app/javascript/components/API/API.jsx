60e44d2af2a6d046e493a7c85d1dbeffb2a0258f
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2019-05-31 11:30:20 +0200

    Make API return Promise from Axios result
    Before this chagne was made the API couldn't process data itself. The API is now "promise-promise" based such that it can process data and send the processed data back instead of letting the components process the data by themselves.

--- app/javascript/components/API/API.jsx
+++ app/javascript/components/API/API.jsx
@@ -9,43 +9,72 @@ export default class API {
 
   static loginUser(email, password) {
     console.log("loginUser called");
-
-    return this.service.post("auth", {
-      session: {
-        email: email,
-        password: password
-      },
-      authenticity_token: window._token
+    return new Promise((resolve, reject) => {
+      this.service.post("auth", {
+        session: {
+          email: email,
+          password: password
+        },
+        authenticity_token: window._token
+      }).then(response => {
+        /* IMPLEMENT LOGIC FOR PROCESSING DATA HERE */
+        resolve(response);
+      }).catch(error => {
+        /* IMPLEMENT LOGIC FOR PROCESSING ERRORS HERE */
+        reject(error);
+      });
     });
   }
   static logoutUser() {
     console.log("logoutUser called");
-    return this.service.delete("logout", {
-      data: {
-        authenticity_token: window._token
-      }
+    return new Promise((resolve, reject) => {
+        this.service.delete("logout", {
+        data: {
+          authenticity_token: window._token
+        }
+      }).then(response => {
+        /* IMPLEMENT LOGIC FOR PROCESSING DATA HERE */
+        resolve(response);
+      }).catch(error => {
+        /* IMPLEMENT LOGIC FOR PROCESSING ERRORS HERE */
+        reject(error);
+      });
     });
   }
   static registerUser(name, email, password, password_confirmation) {
     console.log("registerUser called");
-
-    return this.service.post("users", {
-      user: {
-        name: name,
-        email: email,
-        password: password,
-        password_confirmation: password_confirmation
-      },
-      authenticity_token: window._token
+    return new Promise((resolve, reject) => {
+      this.service.post("users", {
+        user: {
+          name: name,
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation
+        },
+        authenticity_token: window._token
+      }).then(response => {
+        /* IMPLEMENT LOGIC FOR PROCESSING DATA HERE */
+        resolve(response);
+      }).cach(error => {
+        /* IMPLEMENT LOGIC FOR PROCESSING ERRORS HERE */
+        reject(error);
+      });
     });
   }
   static getUserList(offset = 0, limit = 30) {
     console.log("getUserList called");
-
-    return this.service.get("users", {
-      offset: offset,
-      limit: limit,
-      authenticity_token: window._token
+    return new Promise((resolve, reject) => {
+      this.service.get("users", {
+        offset: offset,
+        limit: limit,
+        authenticity_token: window._token
+      }).then(response => {
+        /* IMPLEMENT LOGIC FOR PROCESSING DATA HERE */
+        resolve(response);
+      }).catch(error => {
+        /* IMPLEMENT LOGIC FOR PROCESSING ERRORS HERE */
+        reject(error);
+      });
     });
   }
 }
