5666959d21c9f9bac53715053b50e68bbb2a298c
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-04 15:47:27 +0200

    Add show action

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -1,2 +1,20 @@
 class Api::ExercisesController < ApplicationController
+
+    def show
+        @exercise = Exercise.find(params[:id])
+        render json: @user, status: :ok
+    end
+
+    def index
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+    def update
+    end
+
 end
