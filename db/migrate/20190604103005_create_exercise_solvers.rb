47cef8a440d462a42d322ffe7de7dd9a49ee002a
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-04 12:33:05 +0200

    Generate student-exercise relationship model

---
+++ db/migrate/20190604103005_create_exercise_solvers.rb
@@ -0,0 +1,13 @@
+class CreateExerciseSolvers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :exercise_solvers do |t|
+      t.integer :student_id
+      t.integer :exercise_id
+
+      t.timestamps
+    end
+    add_index :exercise_solvers, :student_id
+    add_index :exercise_solvers, :exercise_id
+    add_index :exercise_solvers, [:follower_id, :follower_id, unique: true]
+  end
+end
