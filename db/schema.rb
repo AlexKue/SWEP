3d74e712a76839fd04c67c9e9e8a00a11af5deb7
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-05 09:24:43 +0200

    Add titles to exercises

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_125614) do
+ActiveRecord::Schema.define(version: 2019_06_05_065940) do
 
   create_table "exercise_solvers", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_125614) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
