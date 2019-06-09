8f926d7a84be5ce293beae3c26879c0bf22d8a2d
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-09 13:24:45 +0200

    Add category presence validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,4 +2,6 @@ class Exercise < ApplicationRecord
     has_many :exercise_solvers
     has_many :users, through: :exercise_solvers
     belongs_to :category
+
+    validates :category_id, presence: true
 end
