b487e160e0034a17e88e419cf0fb5f44ac688777
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-09 13:21:43 +0200

    Test create path

--- test/controllers/api/exercises_controller_test.rb
+++ test/controllers/api/exercises_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class Api::ExercisesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should add exercise to a category" do
+    assert_difference "Exercise.count", 1 do
+      post api_exercises_path, params: {  exercise: { title: "Test",
+                                                    text: "Lorem ipsum",
+                                                    points: 5.0,
+                                                    category_id: categories(:sfw_queries).id }}
+    end
+  end
 end
