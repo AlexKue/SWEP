186b5a9467743203d4f57ac715fc4ffd004e99f4
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-10 20:17:24 +0200

    Use correct url helper

--- test/controllers/api/exercises_controller_test.rb
+++ test/controllers/api/exercises_controller_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class Api::ExercisesControllerTest < ActionDispatch::IntegrationTest
   test "should add exercise to a category" do
     assert_difference "Exercise.count", 1 do
-      post api_exercises_path, params: {  exercise: { title: "Test",
-                                                    text: "Lorem ipsum",
-                                                    points: 5.0,
-                                                    category_id: categories(:sfw_queries).id }}
+      post api_category_exercises_path(categories(:sfw_queries)), params: { exercise: {  title: "Test",
+                                                                text: "Lorem ipsum",
+                                                                points: 5.0}
+                                                }
     end
   end
 end
