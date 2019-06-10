4d19c95877df33933e2edd924de74bc4d0e83c36
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-10 21:02:56 +0200

    Change role to integer

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_04_125614) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercise_solvers", force: :cascade do |t|
     t.integer "user_id"
     t.integer "exercise_id"
