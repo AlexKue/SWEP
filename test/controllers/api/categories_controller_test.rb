eadece2c6497cc8c33cc780105a6732cc5196508
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-10 14:11:21 +0200

    Write simple tests

--- test/controllers/api/categories_controller_test.rb
+++ test/controllers/api/categories_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class Api::CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @category = categories(:sfw_queries)
+  end
+
+  test "should create category" do
+    assert_difference 'Category.count', 1 do
+      post api_categories_path, params: { category: { title: @category.title,
+                                                      text: @category.text
+      }}
+      assert_response :ok
+    end
+  end
+
+  test "should not allow blank titles" do
+    assert_no_difference 'Category.count' do
+      post api_categories_path, params: { category: { title: "   ",
+                                                      text: @category.text
+      }}
+      assert_response :unprocessable_entity
+    end
+  end
+
+  test "should not allow blank text" do
+    assert_no_difference 'Category.count' do
+      post api_categories_path, params: { category: { title: @category.title,
+                                                      text: "   "
+      }}
+      assert_response :unprocessable_entity
+    end
+  end
+
 end
