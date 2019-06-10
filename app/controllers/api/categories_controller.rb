0fc01ca0369c14238b506db1dad756728511a955
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-10 11:53:14 +0200

    Add show, index, create and destroy methods

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,2 +1,30 @@
 class Api::CategoriesController < ApplicationController
+    def show
+        @category = Category.find(params[:id])
+        render json: @category, status: ok
+    end
+
+    def index
+        offset = params[:offset].to_i
+        limit = params[:limit].nil? ? 30 : params[:limit].to_i
+        
+        @categories = Category.offset(offset).limit(limit)
+        render json: @categories, status: :ok
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            render json: @category, status: :ok
+        else
+            render json: @category.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        if category = Category.find_by(id: params[:id])
+            category.destroy
+        end
+        head :no_content
+    end 
 end
