72614509bf5ae12a33ed4a8a6249255c78676b42
Mark Umnus <merkrafter@gmail.com>
2019-06-11 22:18:27 +0200

    Add test case for wrong syntax

--- test/helpers/query_checker_helper_test.rb
+++ test/helpers/query_checker_helper_test.rb
@@ -7,6 +7,13 @@ class QueryCheckerHelperTest < ActionView::TestCase
   test "whitespace insensitivity" do
     query = "SELECT    * FROM   test_db   WHERE a>5;"
     reference = "SELECT * \nFROM test_db\nWHERE a > 5;"
-    assert @checker.check query, reference 
+    assert @checker.check query, reference
   end
+
+  test "syntax check" do
+    query = "SLCT * FRM test_db WHR a > 5;"
+    reference = "SELECT * \nFROM test_db\nWHERE a > 5;"
+    assert_equal @checker.check(query, reference), false
+  end
+
 end
