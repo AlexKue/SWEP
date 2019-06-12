eb17ade0662e296f14863ae4ed905b857aa8ccc9
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-12 11:04:34 +0200

    Move auth-related methods to this file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include Api::SessionsHelper
+
+    def logged_in_user
+        head :unauthorized unless logged_in? 
+    end
+
+    def admin_user
+        head :forbidden unless current_user.admin?
+    end
 end
