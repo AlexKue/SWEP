20505f3b6cb3eae710770c515c18ad6a7b8c9dca
Mark Umnus <merkrafter@gmail.com>
2019-06-12 15:27:30 +0200

    Fix syntax checker to never return true
    This was necessary since the syntactical correctness of a query does not imply its overall correctness.

--- lib/checking/syntax_checker.rb
+++ lib/checking/syntax_checker.rb
@@ -2,8 +2,9 @@ class SyntaxChecker
   ##
   # Returns +true+ if +query+ is a syntactically correct sql query.
   # For now, this method checks whether there is a +SELECT+ and a +FROM+ keyword in +query+.
+  # If that's not the case then this method returns +nil+.
   def check (query, reference)
-    contains(query, "select") && contains(query, "from")
+    contains(query, "select") && contains(query, "from") && nil
   end
 
   def contains query, substring
