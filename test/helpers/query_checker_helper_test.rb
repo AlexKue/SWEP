b9f1bbc4f3487b53e5b4a2b571e2492819037bc9
Mark Umnus <merkrafter@gmail.com>
2019-06-19 17:44:19 +0200

    Adjust tests to new naming

--- test/helpers/query_checker_helper_test.rb
+++ test/helpers/query_checker_helper_test.rb
@@ -7,37 +7,37 @@ class QueryCheckerHelperTest < ActionView::TestCase
   test "case insensitivity" do
     query = "select * from test_db where a > 5;"
     reference = "SELECT * \nFROM test_db\nWHERE a > 5;"
-    assert @checker.check query, reference
+    assert @checker.correct? query, reference
   end
 
   test "whitespace insensitivity" do
     query = "SELECT    * FROM   test_db   WHERE a>5;"
     reference = "SELECT * \nFROM test_db\nWHERE a > 5;"
-    assert @checker.check query, reference
+    assert @checker.correct? query, reference
   end
 
   test "whitespace insensitivity does not affect strings in query" do
     query = "SELECT * FROM professoren WHERE name LIKE 'oliver giften';"
     reference = "SELECT * FROM professoren WHERE name LIKE 'oli vergiften';"
-    assert_nil @checker.check query, reference
+    assert_nil @checker.correct? query, reference
   end
 
   test "(way too) simple syntax check" do
     query = "SLCT * FRM test_db WHR a > 5;"
     reference = "SELECT * \nFROM test_db\nWHERE a > 5;"
-    assert_equal false, @checker.check(query, reference)
+    assert_equal false, @checker.correct?(query, reference)
   end
 
   test "2-element WHERE clause permutation" do
     query = "SELECT * FROM studenten WHERE name LIKE 'oliver' AND semester = 4;"
     reference = "SELECT * FROM studenten WHERE semester = 4 AND name LIKE 'matthias';"
-    assert @checker.check query, reference
+    assert @checker.correct? query, reference
   end
 
   test "forgot one predicate" do
     query = "SELECT * FROM studenten;"
     reference = "SELECT * FROM studenten WHERE name LIKE 'Xenokrates';"
-    assert_equal false, @checker.check(query, reference)
+    assert_equal false, @checker.correct?(query, reference)
   end
 
 end
