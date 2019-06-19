61da868d29f7ee7a05fc4385db2a21621abc9fb4
Mark Umnus <merkrafter@gmail.com>
2019-06-19 15:36:41 +0200

    Implement simple execution based checker

--- app/helpers/query_checker_helper.rb
+++ app/helpers/query_checker_helper.rb
@@ -2,6 +2,7 @@ require 'checking/query_checker'
 require 'checking/whitespace_checker'
 require 'checking/syntax_checker'
 require 'checking/explain_based_checker'
+require 'checking/execution_based_checker'
 
 module QueryCheckerHelper
 
@@ -10,6 +11,7 @@ module QueryCheckerHelper
     query_checker.add_checker WhitespaceChecker.new
     query_checker.add_checker SyntaxChecker.new
     query_checker.add_checker ExplainBasedChecker.new
+    query_checker.add_checker ExecutionBasedChecker.new
     query_checker
   end
 end
--- lib/checking/execution_based_checker.rb
+++ lib/checking/execution_based_checker.rb
@@ -1,18 +1,50 @@
-class ExplainBasedChecker
+class ExecutionBasedChecker
 
-  def check (query, reference)
+  ##
+  # Compares the queries by actually executing them on a test database.
+  # A problem is that the results could come in any order.
+  # This method needs to be insensitive to that while respecting the order if ORDER BY statements are part of any query.
+  # TODO cache reference results
+  def check query, reference, dbname='unidb'
+    query_result = execute query, dbname
+    reference_result = execute reference, dbname
+    
+    if result_eql? query_result, reference_result
+      nil
+    else
+      false
+    end
+
+  end
+
+  ##
+  # This method compares for two PG::Result whether there are equal.
+  # It is drawn out of the `check` method to provide a better testability.
+  def result_eql? query_result, reference_result
+    # result tables must have the same size
+    false if query_result.ntuples != reference_result.ntuples
+    false if query_result.nfields != reference_result.nfields
+    
+    # result tables must not have any different elements
+    false if not (query_result.filter do |row| !reference_result.include? row end).empty?
+    false if not (reference_result.filter do |row| !query_result.include? row end).empty?
+  
+    true
+  end
+
+  def execute query, dbname
     begin
-      con = PG.connect :dbname => 'unidb', :user => 'sqrrl', :password => 'sqrrl' # TODO really change this
+      con = PG.connect :dbname => dbname
       rs = con.exec query # TODO SANITIZE
-      rs.each do |row|
-        puts row # TODO compare with reference
-      end
+      #rs.each do |row|
+      #  puts row # TODO compare with reference
+      #end
     rescue PG::Error => e
       puts e.message
     ensure
-      rs.clear if rs
       con.close if con
     end
+    rs
   end
 
 end
