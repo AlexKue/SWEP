43c941c2551f04d74c3bfdcf8601991440119a51
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-23 20:36:31 +0200

    Remove nested attribute tests

--- test/controllers/api/exercises_controller_test.rb
+++ test/controllers/api/exercises_controller_test.rb
@@ -1,24 +1,18 @@
 require 'test_helper'
 
 class Api::ExercisesControllerTest < ActionDispatch::IntegrationTest
-  test "should add exercise to a category with sample query" do
+  test "should add exercise to a category" do
     log_in_as(users(:Alex))
     assert_difference "Exercise.count", 1 do
-      assert_difference "Query.count", 2 do
-        post api_category_exercises_path(categories(:sfw_queries)), 
-        params: { 
-          exercise: {  
-            title: "Test",
-            text: "Lorem ipsum",
-            points: 5.0,
-            queries_attributes: [
-              { query: "SELECT * FROM table"},
-              { query: "SELECT * FROM table2 WHERE name=test"}
-            ]
-          }
+      post api_category_exercises_path(categories(:sfw_queries)), 
+      params: { 
+        exercise: {  
+          title: "Test",
+          text: "Lorem ipsum",
+          points: 5.0,
         }
+      }
       assert_response :created
-      end
     end
   end
   test "should not add exercise as non-admin" do
@@ -38,30 +32,14 @@ class Api::ExercisesControllerTest < ActionDispatch::IntegrationTest
     @exercise = exercises(:one)
 
     log_in_as users(:Alex)
-    assert_no_difference "@exercise.queries.count" do
-      patch api_exercise_path(@exercise),
-      params: {
-      exercise: {
-        title: "New Title",
-        queries_attributes: [
-          {
-            id: queries(:one).id,
-            query:"SELECT new FROM table"
-          },
-          {
-            id: queries(:two).id,
-            destroy: true
-          },
-          {
-            query: "SELECT table FROM newtable"
-          }
-        ]
-        }
+    patch api_exercise_path(@exercise),
+     params: {
+     exercise: {
+      title: "New Title",
       }
+    }
       assert_response :no_content
       @exercise.reload
       assert_equal @exercise.title, "New Title"
-      assert_equal @exercise.queries.find(queries(:one).id).query, "SELECT new FROM table"
-    end
   end
 end
