4304e82a71f8f0d1c8c9495e67e1b6a68ad360ab
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-23 20:43:36 +0200

    Add index method

--- app/controllers/api/queries_controller.rb
+++ app/controllers/api/queries_controller.rb
@@ -3,4 +3,17 @@ class Api::QueriesController < ApplicationController
         @query = Queries.find(params[:id])
         render json: @query, status: ok
     end
+
+    def index
+        offset = params[:offset].to_i
+        limit = params[:limit].nil? ? 30 : params[:limit].to_i
+
+        @exercise = Exercise.find(params[:exercise_id])
+        @queries = @exercise.queries.offset(offset).limit(limit)
+        render json: {
+            count: @exercise.queries.count,
+            data: @queries.as_json
+        }, status: :ok
+    end
+
 end
