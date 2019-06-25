1ce7b99af61f66a91db9d7ae5572f459f48751a6
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-25 19:23:05 +0200

    Add Query existence validation

--- app/models/student_query.rb
+++ app/models/student_query.rb
@@ -2,4 +2,6 @@ class StudentQuery < ApplicationRecord
     has_one :exercise_solver
     has_one :exercise, through: :exercise_solver
     has_one :user, through: :exercise_solver
+
+    validates :query, presence: true
 end
