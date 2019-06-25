f01fdb29baec61381a2ca46662fd56f79b09035e
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-25 12:48:29 +0200

    Replace generic query with student_query

---
+++ app/models/student_query.rb
@@ -0,0 +1,5 @@
+class StudentQuery < ApplicationRecord
+    has_one :exercise_solver
+    has_one :exercise, through: :exercise_solver
+    has_one :user, through: :exercise_solver
+end
--- db/migrate/20190624104549_add_query_id_to_exersive_solvers.rb
+++ db/migrate/20190624104549_add_query_id_to_exersive_solvers.rb
@@ -1,5 +1,5 @@
 class AddQueryIdToExersiveSolvers < ActiveRecord::Migration[5.2]
   def change
-    add_column :exercise_solvers, :query_id, :integer
+    add_column :exercise_solvers, :student_query_id, :integer
   end
 end
