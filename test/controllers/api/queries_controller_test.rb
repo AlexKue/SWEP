e87e6da30327ae93b7ecd593897438f5f53bd0b7
Mark Umnus <merkrafter@gmail.com>
2019-06-29 17:55:23 +0200

    TEMP COMMIT

--- test/controllers/api/queries_controller_test.rb
+++ test/controllers/api/queries_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class Api::QueriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should accept correct query" do
+       
+    post api_exercise_queries_path params: {
+        query: {
+        query: "SELECT * FROM studenten;"
+      }
+    }
+    
+     # HTTP query: {"query":{"query":"INSERT INTO Here VALUES('QUERY')"}
+     # expected answer: {"id":2,"query":"INSERT INTO Here VALUES('QUERY')","exercise_id":1,"created_at":"2019-06-27T19:49:22.638Z","updated_at":"2019-06-27T19:49:22.638Z"}
+     assert_response :created
+    end
 end
