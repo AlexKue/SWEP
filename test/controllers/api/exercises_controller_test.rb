20693531b74a6801b3425885284d2b8bb29fcd1f
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-06-29 13:15:16 +0200

    Write solve and show tests

--- test/controllers/api/exercises_controller_test.rb
+++ test/controllers/api/exercises_controller_test.rb
@@ -16,17 +16,21 @@ class Api::ExercisesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "should solve an exercise" do
-    log_in_as(users(:Mark))
-    post solve_api_exercise_path(exercises(:one)),
-    params: {
-      query: {
-        query: student_queries(:one).query,
-
+  test "should solve an exercise and update solution" do
+    log_in_as(users(:Matthias))
+      post solve_api_exercise_path(exercises(:one)),
+      params: {
+        query: "SELECT * FROM table1"
       }
-    }
-    assert_response :created
-    puts response.body
+    assert_response :ok
+
+    assert_no_difference "ExerciseSolver.count" do
+      post solve_api_exercise_path(exercises(:one)),
+      params: {
+        query: "SELECT * FROM table"
+      }
+    assert_response :ok
+    end
   end
 
   test "should not add exercise as non-admin" do
