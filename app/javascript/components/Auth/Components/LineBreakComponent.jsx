af53a337ef2eabff65c3d3b8d0b19540e0e57002
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2019-07-05 20:22:22 +0200

    Add Random Key to Linebreak Component

--- app/javascript/components/Auth/Components/LineBreakComponent.jsx
+++ app/javascript/components/Auth/Components/LineBreakComponent.jsx
@@ -4,7 +4,11 @@ export default class LineBreakComponent extends React.Component {
     render() {
         let lines = this.props.text.split("\n");
         return (
-            lines.map((line) => <p>{ line }</p>)
+            lines.map((line) => <p key={Math.random()}>{ line }</p>)
         );
+        /* 
+            Technically it doesn't make sense to use random keys, as there can't be an update then
+            But as this component can be used everywhere, it's safer to use random keys
+        */
     }
 }
