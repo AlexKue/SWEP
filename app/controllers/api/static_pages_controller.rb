b76f7391968cdef4c5bc77b1ea78aa0ece300042
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-07-05 23:04:00 +0200

    Write playground action

--- app/controllers/api/static_pages_controller.rb
+++ app/controllers/api/static_pages_controller.rb
@@ -1,2 +1,9 @@
 class Api::StaticPagesController < ApplicationController
+
+    def playground
+        @query = params[:query]
+        result_table = get_result_table @query
+        render json: result_table, status: :ok
+    end
+
 end
