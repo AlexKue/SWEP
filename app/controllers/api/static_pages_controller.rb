fde12a39b5b377ee7a07055aa18dba7b05ca8224
Alexander-Michael Kühnle <alexander.kuehnle@uni-jena.de>
2019-07-06 17:54:51 +0200

    Add logged_in before action

--- app/controllers/api/static_pages_controller.rb
+++ app/controllers/api/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class Api::StaticPagesController < ApplicationController
 
+    before_action :logged_in_user
+
     def playground
         @query = params[:query]
         result_table = get_result_table @query
