2a9e2499aae6c7349720bfdbc7739498fb3e6b19
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2019-07-06 14:27:38 +0200

    Enhancement: Make API based on origin

--- app/javascript/components/API/API.jsx
+++ app/javascript/components/API/API.jsx
@@ -7,7 +7,7 @@ import {
 export default class API {
 
   static service = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: window.location.origin + "/api/",
     responseType: "json"
   });
 
