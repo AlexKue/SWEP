fdb6f1a5c63277b966866f96af4c5a0ad47b0b23
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2019-07-07 13:49:16 +0200

    Implement: Scoreboard Component

---
+++ app/javascript/components/Auth/Scoreboard/ScoreboardView.jsx
@@ -0,0 +1,102 @@
+import React, { useContext } from "react"
+import {
+    Table, Loader, Icon, Button, Segment
+} from "semantic-ui-react"
+
+import AuthedContext from '../AuthedContext.jsx';
+import API from "../../API/API.jsx";
+import ThreeColumnTable, { ThreeColumnTableRow } from '../Components/ThreeColumnTable.jsx';
+
+const ScoreboardView = (props) => {
+    let context = useContext(AuthedContext);
+
+    return <ScoreboardViewComponent context={context} {...props} />
+}
+
+class ScoreboardViewComponent extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            context: props.context,
+            initialized: false,
+            loadingText: "Lade Scoreboard",
+            scoreboardEntries: []
+        }
+    }
+
+    componentDidMount() {
+        this.initialize(false);
+    }
+
+    initialize = (refresh) => {
+        if (!this.state.initialized || refresh) {
+            API.getScoreboard()
+            .then(response => {
+                let scoreboardDataArray = response.data;
+                let scoreboardEntries = [];
+                if (scoreboardDataArray.length > 0) {
+                    for (let scoreboardEntry of scoreboardDataArray) {  
+                        scoreboardEntries.push(
+                            <Table.Row key={"scb_" + scoreboardEntry.rank }>
+                                <Table.Cell collapsing textAlign="center">
+                                    { scoreboardEntry.rank < 4 ? (
+                                        scoreboardEntry.rank == 1 ? <Icon name="trophy" style={{color: "#FFD700"}} /> :
+                                        scoreboardEntry.rank == 2 ? <Icon name="trophy" style={{color: "#C0C0C0"}} /> : 
+                                        scoreboardEntry.rank == 3 ? <Icon name="trophy" style={{color: "#CD7F32"}} /> : null
+                                        )
+                                    : null}
+                                    { scoreboardEntry.rank }
+                                </Table.Cell>
+                                <Table.Cell>{ scoreboardEntry.name }</Table.Cell>
+                                <Table.Cell collapsing textAlign="center">{ scoreboardEntry.sum }</Table.Cell>
+                            </Table.Row>
+                        );
+                    }
+                    this.setState({scoreboardEntries: scoreboardEntries});
+                }
+            }).catch(error => {
+                console.error(error);
+            }).finally(() => {
+                if (refresh) this.setState({loading: false});
+                else this.setState({initialized: true});
+            });
+        }
+    }
+
+    reload = () => {
+        this.setState({loading: true});
+        this.initialize(true);
+    }
+
+    render() {
+        if (this.state.initialized) {
+            return (
+                <Segment basic loading={ this.state.loading }>
+                        { this.state.scoreboardEntries.length > 0 ?
+                            <Table celled>
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell collapsing>Platz</Table.HeaderCell>
+                                        <Table.HeaderCell>Name</Table.HeaderCell>
+                                        <Table.HeaderCell collapsing textAlign="center">Punkte</Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
+                                <Table.Body>
+                                    { this.state.scoreboardEntries }
+                                </Table.Body>
+                            </Table>
+                            :
+                            <p>Es gibt derzeit keine Einträge in der Rangliste.</p>
+                        }
+                    <Button content="Aktualisieren" icon="refresh" labelPosition="left" color="green" onClick={ this.reload } />
+                </Segment>
+            );
+        } else {
+            return <Loader active inline="centered">{ this.state.loadingText }</Loader>;
+        }
+    }
+}
+
+export default ScoreboardView;
