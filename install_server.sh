4e9ad7ebaad9cfc19310c706772546e0f247dddf
MisterDerpie <34041141+MisterDerpie@users.noreply.github.com>
2019-07-08 10:44:23 +0200

    Change: Redirect traffic both from 80 and 443 to 3001 (HTTPS thin)

--- install_server.sh
+++ install_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # as of https://serverfault.com/questions/112795/how-to-run-a-server-on-port-80-as-a-normal-user-on-linux we redirect all traffic to 3000
 echo "Installing traffic redirect to thin server (80 HTTP; sudo command required)"
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
-sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 3000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3001
+sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 3001
 echo "Installing traffic redirect to thin server (443 HTTPS; sudo command required)"
-sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 3000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 3001
 sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 443 -j REDIRECT --to-ports 3001
 echo "Run server and visit 'localhost' to test if it works"
 
