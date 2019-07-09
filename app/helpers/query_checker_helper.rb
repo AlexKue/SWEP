f07da9158d3ee3e3e578933e86f8058140e878f8
Mark Umnus <merkrafter@gmail.com>
2019-07-09 18:59:37 +0200

    Reactivate explain based checker

--- app/helpers/query_checker_helper.rb
+++ app/helpers/query_checker_helper.rb
@@ -1,6 +1,7 @@
 require 'checking/query_checker'
 require 'checking/whitespace_checker'
 require 'checking/syntax_checker'
+require 'checking/explain_based_checker'
 require 'checking/execution_based_checker'
 
 module QueryCheckerHelper
@@ -9,6 +10,7 @@ module QueryCheckerHelper
     query_checker = QueryChecker.new
     query_checker.add_checker WhitespaceChecker.new
     query_checker.add_checker SyntaxChecker.new
+    query_checker.add_checker ExplainBasedChecker.new
     query_checker.add_checker ExecutionBasedChecker.new
     query_checker
   end
