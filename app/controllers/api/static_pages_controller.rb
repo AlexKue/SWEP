910eb6593f85bcc72ca927642323e2d741d87235
Mark Umnus <merkrafter@gmail.com>
2019-07-10 14:56:16 +0200

    Respond with correctly formatted JSON object on playground

--- app/controllers/api/static_pages_controller.rb
+++ app/controllers/api/static_pages_controller.rb
@@ -7,9 +7,9 @@ class Api::StaticPagesController < ApplicationController
 
         result = get_result_table @query, "unidb", indicate_error=true
         if result.has_key? :error
-          render json: result[:error], status: :unprocessable_entity
+          render json: {error: result[:error]}, status: :unprocessable_entity
         else
-          render json: result[:result], status: :ok
+          render json: {result: result[:result]}, status: :ok
         end
 
     end
