1e69ebf7c400f7be692a4c59e63198647461efa3
Mark Umnus <merkrafter@gmail.com>
2019-07-10 08:36:19 +0200

    Create tests for psql special commands

--- test/controllers/api/static_pages_controller_test.rb
+++ test/controllers/api/static_pages_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class Api::StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @admin = users(:Alex)
+    @student = users(:Mark)
+  end
+
+  test "should not be able to use \\dt" do
+    log_in_as @student
+    post api_playground_path,
+    params: {
+      query: "\\dt"
+    }
+    assert response.body.eql? "[]", response.body
+  end
+
+  test "should not be able to use \\du" do
+    log_in_as @student
+    post api_playground_path,
+    params: {
+      query: "\\du"
+    }
+    assert response.body.eql? "[]", response.body
+  end
 end
